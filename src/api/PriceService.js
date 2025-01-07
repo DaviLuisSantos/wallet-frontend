5c8f6630c58222e98c27a637d54a53a52eb2e4c5
Davi Luis <costasantos.daviluis@gmail.com>
2025-01-07 18:09:41 -0300

    Req mudada

--- src/api/PriceService.js
+++ src/api/PriceService.js
@@ -1,13 +1,13 @@
 import apiClient from "./apiClient";
 
 export const getPricesId = async (ids, startTime, endTime) => {
-    const idsString = ids.join(',');
-    const response = await apiClient.get(`/api/Price/${encodeURIComponent(idsString)}/${encodeURIComponent(startTime)}/${encodeURIComponent(endTime)}`);
+    const idsString = ids.map(id => `ids=${id}`).join('&');
+    const response = await apiClient.get(`/api/Price/filter?${idsString}&startTime=${encodeURIComponent(startTime)}&endTime=${encodeURIComponent(endTime)}`);
     return response.data;
 };
 
 export const getPriceIds = async (ids) => {
     const queryString = ids.map(id => `ids=${id}`).join('&');
-    const response = await apiClient.get(`/api/Price?${queryString}`);
+    const response = await apiClient.get(`/api/Price/byids?${queryString}`);
     return response.data;
 };
